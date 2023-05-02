51d7d97df16864966c537e2401394849e5c70861
jaemin8852 <jaemin8852@naver.com>
2023-05-02 21:31:30 +0900

    edit something

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Let's start~
         </a>
       </header>
     </div>
